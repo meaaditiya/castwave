13ec9301fc3ad92ffa5343b1278f84d4add17864
Aaditiya Tyagi <aaditiyatyagi123@gmail.com>
2025-08-11 17:11:44 +0000

    restore original styles man , the chatroom compoennt is nothign at all s

--- src/components/LiveChat.tsx
+++ src/components/LiveChat.tsx
@@ -337,7 +337,7 @@ export function LiveChat({ chatRoom, messages, participant, canChat }: LiveChatP
   return (
     <>
       <CardContent className="flex-1 flex flex-col p-2 sm:p-4 min-h-0">
-        <div className="flex-1 min-h-0 relative">
+        <div className="flex-1 relative min-h-0">
           <RemoveScroll enabled>
               <ScrollArea className="h-full pr-4" viewportRef={scrollViewportRef}>
                   <div className="space-y-4">
